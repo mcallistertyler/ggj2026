63d0d838aa1632b218ecea035753e274a730a5dc
byllgrim <myemail>
2026-01-31 00:16:06 +0100

    let 'location entry' points have configurable textures

--- script_location_entry.gd
+++ script_location_entry.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@export var my_texture : CompressedTexture2D
+
 func _ready():
+	%MySprite.texture = my_texture
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
