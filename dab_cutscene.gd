b606afb5fe7bf33e212a585fbc6c53d55524e521
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2026-01-31 20:39:55 +0100

    dab animation

---
+++ dab_cutscene.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+#see assets/character_sprites/dabup
+@export var npc_texture : Texture2D
+@export var hand_texture : Texture2D
+@export var success_texture : Texture2D
+
+
+@onready var npc_portrait = %NPCPortrait
+@onready var hand_image = %NPCHand
+@onready var player_portait = %PlayerPortrait
+@onready var success_portrait = %SuccessPortrait
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	if npc_texture == null:
+		push_error("YOU FUCKER YOU FORGOT TO SET UP THE DAB SCENE")
+	npc_portrait.texture = npc_texture
+	hand_image.texture = hand_texture
+	success_portrait.texture = success_texture
+
+func play_dab_animation():
+	$AnimationPlayer.play("dab_me_up")
+
+func play_success_animation():
+	$AnimationPlayer.play("dab_success")
