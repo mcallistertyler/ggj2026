f642ef8dcc37354787f78352d5d647e364999624
Ty <tytheguy95@gmail.com>
2026-02-01 00:55:19 +0100

    move kioskmann logic to a shared script. create remaining npcs (#45)

--- NPCs/Homeless/homeless_fox.gd
+++ NPCs/Homeless/homeless_fox.gd
@@ -5,7 +5,8 @@ extends NPC
 
 func _ready() -> void:
 	super._ready()
-	homeless_cup_area_3d.body_entered.connect(_on_homeless_cup_body_entered)
+	if homeless_cup_area_3d:
+		homeless_cup_area_3d.body_entered.connect(_on_homeless_cup_body_entered)
 	
 func _on_homeless_cup_body_entered(body: Node3D) -> void:
 	if body.is_in_group(Groups.PLAYER_GROUP):
