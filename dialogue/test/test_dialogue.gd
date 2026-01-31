c5f3197f36a51d79e100c81cde5985043fc223de
Tyler <tyler.mcallister@aidn.no>
2026-01-31 01:25:54 +0100

    create custom dialogue box and add test dialogue

---
+++ dialogue/test/test_dialogue.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var dialogue_resource : DialogueResource
+@export var dialogue_title : String
+
+var dialogue_open : bool = false
+
+func _ready() -> void:
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.got_dialogue.connect(_on_dialogue_line)
+	DialogueManager.passed_title.connect(_on_dialogue_title)
+
+func _on_dialogue_ended(resource: DialogueResource) -> void:
+	print("Dialogue ended ", resource)
+
+func _on_dialogue_started(resource: DialogueResource) -> void:
+	print("Dialogue started ", resource)
+	
+func _on_dialogue_line(line) -> void:
+	print("Dialogue line ", line)
+	
+func _on_dialogue_title(title) -> void:
+	print("Dialogue title ", title)
+
+func _on_spoke(letter: String, letter_index: int, speed: float) -> void:
+	print("Letter ", letter, "\nLetter index ", letter_index, "\nSpeed ", str(speed))
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and !dialogue_open:
+		dialogue_open = true
+		var dialogue_balloon : DialogueBaloon = DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
+		await dialogue_balloon.ready
+		dialogue_balloon.dialogue_label.spoke.connect(_on_spoke)
+		await DialogueManager.dialogue_ended
+		dialogue_open = false
