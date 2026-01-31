66ef1dfd3cf3a4ec288bf91a502cc602ece38974
Tyler <tyler.mcallister@aidn.no>
2026-01-31 02:12:18 +0100

    add player billboard

---
+++ Player/player_character_body_3d.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+class_name PlayerCharacterBody3D
+
+@export var speed : float = 5.0
+@export var gravity : float = 9.8
+@export var animated_sprite : AnimatedSprite3D
+@export var animation_player : AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		if animation_player.current_animation != "walk":
+			animation_player.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+	if direction.x != 0:
+		animated_sprite.flip_h = direction.x < 0
+	self.move_and_slide()
