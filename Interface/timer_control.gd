9cfed1c8ec2691f2d9cc3fe6331b9842cb33efd1
Ty <tytheguy95@gmail.com>
2026-01-31 15:25:12 +0100

    Dialogue response logic and global timer autoload (#13)
    * add dialogue wioh timer and improvements to response container as well as test code for signals from balloon and dialogue manager
    
    * inconsequential changes to narvesen
    
    * add time manager for dialogue
    
    * make signal for responses being shown

---
+++ Interface/timer_control.gd
@@ -0,0 +1,27 @@
+extends Control
+
+signal timed_out
+
+@onready var timer_text : RichTextLabel = get_node("%TimerText")
+#TODO: I don't care about the progress bar yet
+@onready var progress_bar : ProgressBar = get_node("%ProgressBar")
+
+var timer_active : bool = false
+
+func _ready() -> void:
+	self.hide()
+	TimerManager.start_dialogue_timer.connect(_on_start_dialogue_timer)
+	TimerManager.timer_manager_timeout.connect(_on_timer_timeout)
+
+func _process(_delta: float) -> void:
+	if timer_active:
+		var time_left = TimerManager.get_time_left()
+		timer_text.text = "%.2f" % time_left
+
+func _on_start_dialogue_timer() -> void:
+	timer_active = true
+	self.show()
+
+func _on_timer_timeout(_context: TimerManager.Context) -> void:
+	timer_active = false
+	self.hide()
---
+++ dialogue/dialogue_with_timer.gd
@@ -0,0 +1,34 @@
+extends Node
+class_name DialogueWithTimer
+
+@export var dialogue_resource : DialogueResource
+@export var dialogue_title : String
+@export var timeout_value : float = 2.0 # set this yourself
+
+enum DialogueState { TIMED_OUT, GAME_SUCCESS, GAME_FAILED }
+
+signal dialogue_state_result(response_tag)
+
+func start_timer() -> void:
+	TimerManager.create_dialogue_timer(timeout_value, TimerManager.Context.DIALOGUE)
+
+func _on_responses_shown(_responses: Array) -> void:
+	start_timer()
+
+func _ready() -> void:
+	TimerManager.timer_manager_timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout(context: TimerManager.Context) -> void:
+	if context == TimerManager.Context.DIALOGUE:
+		dialogue_state_result.emit(DialogueState.TIMED_OUT)
+
+func _on_response_selected(response: DialogueResponse) -> void:
+	var response_tag = Enums.get_dialogue_response_tag(response)
+	if response_tag:
+		dialogue_state_result.emit(response_tag)
+
+func start_dialogue() -> void:
+	var dialogue_balloon : DialogueBaloon = DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_title)
+	await dialogue_balloon.ready
+	dialogue_balloon.responses_shown.connect(_on_responses_shown)
+	dialogue_balloon.responses_menu.response_selected.connect(_on_response_selected)
