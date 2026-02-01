8532934ca427fed5a3e92da2e7fd9a4debb27fe9
hannnst <hannanikolines@outlook.com>
2026-02-01 12:55:16 +0100

    set icons based on difficulty

--- dialogue/dialogue_box/response_container.gd
+++ dialogue/dialogue_box/response_container.gd
@@ -1,6 +1,13 @@
 extends HBoxContainer
 class_name ResponseContainer
 
+var response: DialogueResponse:
+	set(value):
+		response = value
+		_update_icon_and_text()
+	get:
+		return response
+
 var text: String:
 	set(value):
 		var response_button = get_node_or_null("ResponseExample")
@@ -8,3 +15,27 @@ var text: String:
 	get:
 		var response_button = get_node_or_null("ResponseExample")
 		return response_button.text
+
+
+func _update_icon_and_text() -> void:
+	if response == null:
+		return
+	
+	# Update the text
+	text = response.text
+	
+	# Update the icon based on response difficulty
+	var icon_rect = get_node_or_null("AnimatedTextureRect")
+	if icon_rect == null:
+		return
+	
+	var response_tag = Enums.get_dialogue_response_tag(response)
+	match response_tag:
+		Enums.ResponseTag.EASY_MODE:
+			icon_rect.texture = preload("res://assets/ui/yikes.png")  # Easy = yikes icon
+		Enums.ResponseTag.HARD_MODE:
+			icon_rect.texture = preload("res://assets/ui/solbrille.png")  # Hard = sunglasses icon
+		Enums.ResponseTag.FAILURE:
+			icon_rect.texture = preload("res://assets/ui/credzcoin.png")  # Failure icon
+		_:
+			icon_rect.texture = preload("res://assets/ui/credzcoin.png")  # Default icon
