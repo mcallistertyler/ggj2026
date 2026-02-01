2178f39cd7a471a775dc6323036462993e5f3c62
hannnst <hannanikolines@outlook.com>
2026-02-01 16:07:08 +0100

    mp3 convert

--- addons/audio_manager/audio_manager.gd
+++ addons/audio_manager/audio_manager.gd
@@ -40,9 +40,9 @@ var music = {
 	"ambience-gym": preload("res://audio/music/ambience_gym.mp3"),
 	"ambience-narvesen": preload("res://audio/music/ambience_narvesen.mp3"),
 	"ending-good": preload("res://audio/music/ending_good.mp3"),
-	"ending-neutral": preload("res://audio/music/ending_neutral.wav"),
+	"ending-neutral": preload("res://audio/music/ending_neutral.mp3"),
 	"ending-bad": preload("res://audio/music/ending_bad.mp3"),
-	"ending-terrible": preload("res://audio/music/ending_terrible.wav"),
+	"ending-terrible": preload("res://audio/music/ending_terrible.mp3"),
 }
 
 var sfx_players = []
