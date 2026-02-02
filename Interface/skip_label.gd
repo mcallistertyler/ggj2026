4f443053d387390f57e080697dea466f31373b67
Tyler <tyler.mcallister@aidn.no>
2026-02-02 19:35:58 +0100

    use better colour

--- Interface/skip_label.gd
+++ Interface/skip_label.gd
@@ -7,7 +7,7 @@ var is_intro : bool = false
 var fill_tween: Tween
 var pulse_tween: Tween
 var base_text: String = ""
-const FILL_COLOR := "5080ff"
+const FILL_COLOR := "fffdf1"
 
 func _ready() -> void:
 	base_text = text
